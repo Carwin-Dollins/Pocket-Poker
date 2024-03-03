b0006a00b4dbe4bda593987c1432734146b241d1
Darwin Collins <darwincollinsjr@hotmail.com>
2024-03-02 22:11:45 -0600

    Supabase changes, some dummy info to read

--- pocket-poker/src/App.js
+++ pocket-poker/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient("https://hplnxayyijxebyklpoiz.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwbG54YXl5aWp4ZWJ5a2xwb2l6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk0MTQyMjcsImV4cCI6MjAyNDk5MDIyN30.8XJMoHKCQp_dWVaK1p73HtUQlGWWEz2hu-nAi7gNr8s");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [player_stats, setPlayer] = useState([]);
+
+    useEffect(() => {
+      getPlayer();
+    }, []);
+
+    async function getPlayer() {
+      const { data } = await supabase.from("player_stats").select();
+      setPlayer(data);
+    }
+
+    return (
+      <ol>
+        {player_stats.map((player) => (
+          <><li key={player.username}>{player.username}</li><ul>
+            <li key={player.name}>name: {player.name}</li>
+            <li key={player.email}>email: {player.email}</li>
+            <li key={player.id}>id: {player.id}</li>
+            <li key={player.wins}>wins: {player.wins}</li>
+          </ul></>
+          
+        ))}
+      </ol>
+    );
 }
 
 export default App;
